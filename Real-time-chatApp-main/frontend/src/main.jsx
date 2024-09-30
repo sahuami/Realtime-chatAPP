e2858f630dd6bde138d566c9ab0f42bde1f06f19
Amit Sahu <sahuami2000@gmail.com>
2024-09-30 16:34:21 +0530

    add new

---
+++ Real-time-chatApp-main/frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist';
+let persistor = persistStore(store);
+
+export const BASE_URL="http://localhost:8080"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
